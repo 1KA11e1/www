64997dd3193b56ae23feaaf8e5da07f95ea183ca
1KA11e1 <103613210+1KA11e1@users.noreply.github.com>
2023-05-15 12:54:43 -0300

    Delete gepe directory

--- gepe/storage/framework/views/c5a4283ba9057bfffee946cd3fb55aae.php
+++
@@ -1,8 +0,0 @@
-            <!-- To the right -->
-            <div class="float-right d-none d-sm-inline">
-                Anything you want
-            </div>
-            <!-- Default to the left -->
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
-            reserved.
-<?php /**PATH C:\laragon\www\gepe\resources\views/template/footer.blade.php ENDPATH**/ ?>
